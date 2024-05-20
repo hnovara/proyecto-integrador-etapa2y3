49f9ec48cd96ba95d6137a8a559c9fab2243698b
hnovara <her.novara@hotmail.com>
2024-05-20 00:15:09 -0300

    FIX CartItem

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -6,9 +6,7 @@ function CartItem({product, quantity}) {
         <div className='cart-item__container'>
             <h2>
                 {product.name}
-                <span>({product.brand})</span>
             </h2>
-            <p>{product.category}</p>
             <Counter 
                 _id={product._id}
                 initialValue={quantity}/>
